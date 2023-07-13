237d5c1c569273e0658161b33989afdeee1f1fec
iRobot98 <matendejudah@gmail.com>
2023-07-13 17:07:13 +0300

    working on sunflower app

--- views/auth/src/pages/signup.jsx
+++ views/auth/src/pages/signup.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from "react";
+import Layout from "../components/layout";
 
-function SignUp(props) {
-  return (
-    <Layout>
-      <div className="form_ ">
-        <img src="/assets/images/logo.png" className='h-[50px] w-[50px]' alt="" srcset="" />
-      </div>
-    </Layout>
-  )
+function FormInput({ name, label, value, type, placeholder }) {
+    return (
+        <div className="flex-center m-2 ">
+            {label && <label htmlFor={name}>{label}</label>}
+            <input
+                className="mx-2 rounded-md p-[.2rem]"
+                type={type}
+                id={name}
+                name={name}
+                onChange={(e) => (value = e.target.value)}
+                placeholder={placeholder}
+            />
+        </div>
+    );
 }
 
-export default SignUp
+function SignUp(props) {
+    const data = {
+        username: "",
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+        console.log(data);
+    };
+    return (
+        <Layout>
+            <div className="form_ ">
+                <div>
+                    <form
+                        className="bg-[grey] min-h-[5rem] rounded-md flex justify-between flex-col"
+                        onSubmit={onSubmit}
+                    >
+                        <FormInput
+                            name="username"
+                            type="text"
+                            value={data["username"]}
+                            label="username"
+                        />
+
+                        <button
+                            className="p-2 bg-red-700 text-white rounded-md"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default SignUp;
