cb38fc0a4c73530eaa3058d2bd2774c26b889e8a
iRobot98 <matendejudah@gmail.com>
2023-07-15 13:13:31 +0300

    working on sunflower app

--- app.js
+++ app.js
@@ -13,50 +13,8 @@ app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use("*", require("./src"));
 
-app.get("*", (req, res, callNext) => {
-    const { originalUrl, method } = req;
+app.use("/auth", require("./src/auth"));
 
-    console.log(originalUrl);
-    switch (originalUrl.toLowerCase()) {
-        case "/auth":
-        case "/auth/":
-            return res.redirect("/auth/sign_in");
-
-        case "/auth/404":
-        case "/auth/sign_up":
-        case "/auth/sign_in":
-            // res.contentType("html");
-            sendFile("./views/auth/build/index.html")
-                .then((f_) => res.contentType("html").send(f_))
-                .catch((v) => res.status(404).redirect("/404"));
-            return;
-        case "/sign_in":
-            res.redirect("/auth/sign_in");
-            return;
-
-        case "/sign_up":
-            res.redirect("/auth/sign_up");
-            return;
-        default:
-            if (!originalUrl.toLowerCase().startsWith("/auth"))
-                return callNext();
-    }
-
-    let j = "/auth".length;
-    const { split, ext } = splitUrl(originalUrl);
-    if (originalUrl.startsWith("/auth")) {
-        let k = originalUrl.slice(j);
-
-        let j_ = searchFile("views/auth/build", k);
-        if (j_ != "not found") {
-            return res.contentType(ext).send(j_);
-        }
-        res.redirect("../404");
-        console.log(j_);
-        return;
-    }
-    res.status(404);
-});
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
---
+++ src/auth.js
@@ -0,0 +1,50 @@
+const router = require("./router.import");
+const { splitUrl, sendFile } = require("./utils");
+
+router.get("*", (req, res, callNext) => {
+    console.log("new");
+    const { originalUrl, method } = req;
+
+    console.log(originalUrl);
+    switch (originalUrl.toLowerCase()) {
+        case "/auth":
+        case "/auth/":
+            return res.redirect("/auth/sign_in");
+
+        case "/auth/404":
+        case "/auth/sign_up":
+        case "/auth/sign_in":
+            // res.contentType("html");
+            sendFile("./views/auth/build/index.html")
+                .then((f_) => res.contentType("html").send(f_))
+                .catch((v) => res.status(404).redirect("/404"));
+            return;
+        case "/sign_in":
+            res.redirect("/auth/sign_in");
+            return;
+
+        case "/sign_up":
+            res.redirect("/auth/sign_up");
+            return;
+        default:
+            if (!originalUrl.toLowerCase().startsWith("/auth"))
+                return callNext();
+    }
+
+    let j = "/auth".length;
+    const { split, ext } = splitUrl(originalUrl);
+    if (originalUrl.startsWith("/auth")) {
+        let k = originalUrl.slice(j);
+
+        let j_ = searchFile("views/auth/build", k);
+        if (j_ != "not found") {
+            return res.contentType(ext).send(j_);
+        }
+        res.redirect("../404");
+        console.log(j_);
+        return;
+    }
+    res.status(404);
+});
+
+module.exports = router;
